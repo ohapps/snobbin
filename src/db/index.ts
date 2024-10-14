1eace475c89489d03968a47830ff38eae9fc388f
Craig Hausner <craighausner@gmail.com>
2024-10-13 21:25:20 -0500

    run migrations on startup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,18 @@
-// import { config } from 'dotenv';
 import postgres from "postgres";
 import { drizzle } from 'drizzle-orm/postgres-js';
-
-// config({ path: '.env' }); // or .env.local
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
 
 const client = postgres(process.env.DATABASE_URL!);
 export const db = drizzle(client);
+
+const main = async () => {
+    try {
+        await migrate(db, { migrationsFolder: 'migrations' });
+        console.log('Migration completed');
+    } catch (error) {
+        console.error('Error during migration:', error);
+        process.exit(1);
+    }
+};
+
+main();
