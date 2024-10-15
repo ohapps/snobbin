6dac14608ca6d24e415cd12091589c2c1b1caa2b
Craig Hausner <craighausner@gmail.com>
2024-10-14 19:25:31 -0500

    fix migrations

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,18 +1,5 @@
 import postgres from "postgres";
 import { drizzle } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
 
 const client = postgres(process.env.DATABASE_URL!);
 export const db = drizzle(client);
-
-const main = async () => {
-    try {
-        await migrate(db, { migrationsFolder: 'migrations' });
-        console.log('Migration completed');
-    } catch (error) {
-        console.error('Error during migration:', error);
-        process.exit(1);
-    }
-};
-
-main();
