9a34007554df8189b601c577b892d8f1addc460e
Craig Hausner <craighausner@gmail.com>
2025-04-19 20:18:02 -0500

    update loading to default to full height

--- src/components/Page/LoadingPage.tsx
+++ src/components/Page/LoadingPage.tsx
@@ -1,14 +1,14 @@
-"use client";
+'use client';
 
-import { Box, CircularProgress } from "@mui/material";
+import { Box, CircularProgress } from '@mui/material';
 
-const LoadingPage = ({ fullHeight = false }: { fullHeight?: boolean }) => {
+const LoadingPage = ({ fullHeight = true }: { fullHeight?: boolean }) => {
   return (
     <Box
       display="flex"
       alignItems="center"
       justifyContent="center"
-      minHeight={fullHeight ? "100vh" : "200px"}
+      minHeight={fullHeight ? '100vh' : '200px'}
     >
       <CircularProgress size="3rem" />
     </Box>
--- src/components/RankingItem/ItemList.tsx
+++ src/components/RankingItem/ItemList.tsx
@@ -1,18 +1,18 @@
-"use client";
+'use client';
 
-import { PaginatedResults } from "@/types/rankings";
-import { SnobGroup } from "@/types/snobGroup";
-import { Box, Typography } from "@mui/material";
-import Grid from "@mui/material/Grid2";
-import SearchBox from "./SearchBox";
-import ItemListPage from "./ItemListPage";
-import { PagingNavigation } from "./PagingNavigation";
-import SortByMenu from "./SortByMenu";
-import NewItemButton from "./NewItemButton";
-import { formatNumber } from "@/utils/format-number";
-import { useUpdateQueryParams } from "@/hooks/useUpdateQueryParams";
-import { useTransition } from "react";
-import LoadingPage from "../Page/LoadingPage";
+import { PaginatedResults } from '@/types/rankings';
+import { SnobGroup } from '@/types/snobGroup';
+import { Box, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid2';
+import SearchBox from './SearchBox';
+import ItemListPage from './ItemListPage';
+import { PagingNavigation } from './PagingNavigation';
+import SortByMenu from './SortByMenu';
+import NewItemButton from './NewItemButton';
+import { formatNumber } from '@/utils/format-number';
+import { useUpdateQueryParams } from '@/hooks/useUpdateQueryParams';
+import { useTransition } from 'react';
+import LoadingPage from '../Page/LoadingPage';
 
 const ItemList = ({
   group,
@@ -36,8 +36,8 @@ const ItemList = ({
         <Grid
           size={{ md: 6, xs: 12 }}
           paddingBottom={2}
-          display={"flex"}
-          alignItems={"center"}
+          display={'flex'}
+          alignItems={'center'}
         >
           <Typography variant="h5">
             {formatNumber(paginatedResults.total)} items
@@ -45,32 +45,32 @@ const ItemList = ({
         </Grid>
         <Grid
           size={{ md: 6, xs: 12 }}
-          display={"flex"}
-          sx={{ justifyContent: { xs: "flex-start", md: "flex-end" } }}
+          display={'flex'}
+          sx={{ justifyContent: { xs: 'flex-start', md: 'flex-end' } }}
           paddingBottom={2}
         >
           <SearchBox updateQuery={updateQuery} />
         </Grid>
         <Grid
           size={{ md: 6, xs: 9 }}
-          display={"flex"}
-          justifyContent={"flex-start"}
-          alignItems={"center"}
+          display={'flex'}
+          justifyContent={'flex-start'}
+          alignItems={'center'}
           paddingBottom={2}
         >
           <SortByMenu updateQuery={updateQuery} />
         </Grid>
         <Grid
           size={{ md: 6, xs: 3 }}
-          display={"flex"}
-          sx={{ justifyContent: "flex-end" }}
+          display={'flex'}
+          sx={{ justifyContent: 'flex-end' }}
           paddingBottom={2}
         >
           <NewItemButton />
         </Grid>
       </Grid>
       {loading ? (
-        <LoadingPage />
+        <LoadingPage fullHeight={false} />
       ) : (
         <ItemListPage group={group} items={paginatedResults.items} />
       )}
