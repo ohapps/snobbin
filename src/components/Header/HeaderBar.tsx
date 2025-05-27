4a3e99ac02fe5feaba5a46e66380ac8007302c33
Craig Hausner <craighausner@gmail.com>
2025-05-26 22:30:45 -0500

    make header sticky

--- src/components/Header/HeaderBar.tsx
+++ src/components/Header/HeaderBar.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import { AppBar, styled } from '@mui/material';
-import { Toolbar as MuiToolbar } from '@mui/material';
-import Logo from './Logo';
-import MainNavigation from './MainNavigation';
-import NavigationMenu from './NavigationMenu';
-import UserMenu from './UserMenu';
+import { AppBar, styled } from "@mui/material";
+import { Toolbar as MuiToolbar } from "@mui/material";
+import Logo from "./Logo";
+import MainNavigation from "./MainNavigation";
+import NavigationMenu from "./NavigationMenu";
+import UserMenu from "./UserMenu";
 
-export const HeaderItemsWrapper = styled('div')(() => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+export const HeaderItemsWrapper = styled("div")(() => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 export const Toolbar = styled(MuiToolbar)(() => ({
-  justifyContent: 'space-between',
+  justifyContent: "space-between",
 }));
 
 const HeaderBar = () => {
